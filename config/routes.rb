3d363fd8f45bf1f184f172d310f8a70524cd499a
Marcus <marcusferreira23@gmail.com>
2022-06-30 11:11:54 -0700

    add information to pets tables.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources: pets
+  resources :pets
 end
--- db/migrate/20220630171840_add_pets_table.rb
+++ db/migrate/20220630171840_add_pets_table.rb
@@ -1,4 +1,10 @@
 class AddPetsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :pets do |t|
+      t.column :name, :string
+      t.column :species, :string
+
+      t.timestamps
+    end
   end
 end
