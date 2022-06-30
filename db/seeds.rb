c6748a52a501ccf33d4bca36bfff0150d2f0ad50
Marcus <marcusferreira23@gmail.com>
2022-06-30 11:46:27 -0700

    seed database with cats and dogs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    Pet.destroy_all
+    seed = Seed.new
+    seed.generate_pets
+  end
+
+  def generate_pets
+    20.times do |i|
+      pet = Pet.create!(
+        name: Faker::Creature::Dog.name,
+        species: "Dog"
+      )
+      puts "Pet #{i}: #{pet.name} is a #{pet.species}."
+    end
+    20.times do |i|
+      pet = Pet.create!(
+        name: Faker::Creature::Cat.name,
+        species: "Cat"
+      )
+      puts "Pet #{i}: #{pet.name} is a #{pet.species}."
+    end
+  end
+end
+
+Seed.begin
